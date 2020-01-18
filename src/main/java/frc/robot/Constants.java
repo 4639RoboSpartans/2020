31c2827e175ff71192ca9d0716c00b29f8e0714f
Saarthak Mohan <saarthak.mohan@gmail.com>
2020-01-17 22:21:02 -0600

    Refactored OI to use enums

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,49 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    public static final double DEADZONE_VALUE = 0.01;
+    public static final int NUMBER_OF_CONTROLLERS = 2;
+
+    enum Axes {
+        LEFT_STICK_X(0),
+        LEFT_STICK_Y(1),
+        LEFT_TRIGGER(2),
+        RIGHT_TRIGGER(3),
+        RIGHT_STICK_X(4),
+        RIGHT_STICK_Y(5);
+
+        private final int value;
+
+        private Axes(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    enum Buttons {
+        A_BUTTON(0),
+        B_BUTTON(1),
+        X_BUTTON(2),
+        Y_BUTTON(3),
+        LEFT_BUMPER(4),
+        RIGHT_BUMPER(5),
+        BACK_BUTTON(6),
+        START_BUTTON(7),
+        LEFT_STICK(8),
+        RIGHT_STICK(9);
+
+        private final int value;
+
+        private Buttons(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+    }
 }
--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import static frc.robot.Constants.*;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -8,18 +9,13 @@ public class OI {
     private final Joystick[] sticks;
     private final Button[][] buttons;
 
-    // port values of the axes/buttons
-    private static final int leftJoystickX = 0, leftJoystickY = 1, leftTrigger = 2, rightTrigger = 3,
-            rightJoystickX = 4, rightJoystickY = 5;
-
-    private static double deadzoneValue = 0.01;
-
     public OI() {
-        buttons = new JoystickButton[6][8];
-        sticks = new Joystick[2];
-        for (int i = 0; i < sticks.length; i++) {
+        buttons = new JoystickButton[NUMBER_OF_CONTROLLERS][10]; //10 buttons
+        sticks = new Joystick[NUMBER_OF_CONTROLLERS];
+
+        for (int i = 0; i < NUMBER_OF_CONTROLLERS; i++) {
             sticks[i] = new Joystick(i);
-            for (int j = 0; j < buttons.length; j++) {
+            for (int j = 0; j < buttons[i].length; j++) {
                 buttons[i][j] = new JoystickButton(sticks[i], j);
             }
         }
@@ -30,7 +26,7 @@ public class OI {
      * and so there is a margin of error for where they should be considered
      * "neutral/not pushed"
      *
-     * @param d
+     * @param value
      *            Double between -1 and 1 to be deadzoned
      * @return The deadzoned value
      */
@@ -38,7 +34,7 @@ public class OI {
         // whenever the controller moves LESS than the magic number, the
         // joystick is in the loose position so return zero - as if the
         // joystick was not moved
-        if (Math.abs(value) < deadzoneValue) {
+        if (Math.abs(value) < Constants.DEADZONE_VALUE) {
             return 0;
         }
 
@@ -46,40 +42,18 @@ public class OI {
         // that the point right after the deadzone
         // is 0 so the robot does not jerk forward when it passes the deadzone
         // It properly scales the controls to the new workable zone
-        return (value / Math.abs(value)) * ((Math.abs(value) - deadzoneValue) / (1 - deadzoneValue));
+        return (value / Math.abs(value)) * ((Math.abs(value) - DEADZONE_VALUE) / (1 - DEADZONE_VALUE));
     }
 
-    public double getLeftX(int joystick) {
-        double valueLeftX = sticks[joystick].getRawAxis(leftJoystickX);
-        return deadzone(valueLeftX);
+    public double getAxis(int joystick, Axes axis) {
+        double value = sticks[joystick].getRawAxis(axis.getValue());
+        if (axis == Axes.LEFT_STICK_Y || axis == Axes.RIGHT_STICK_Y)
+            return deadzone(-value);
+        else
+            return deadzone(value);
     }
 
-    public double getLeftY(int joystick) {
-        double valueLeftY = sticks[joystick].getRawAxis(leftJoystickY);
-        return deadzone(-valueLeftY);
-    }
-
-    public double getRightX(int joystick) {
-        double valueRightX = sticks[joystick].getRawAxis(rightJoystickX);
-        return deadzone(valueRightX);
-    }
-
-    public double getRightY(int joystick) {
-        double valueRightY = sticks[joystick].getRawAxis(rightJoystickY);
-        return deadzone(-valueRightY);
-    }
-
-    public double getLeftTrigger(int joystick) {
-        double valueLeftTrigger = sticks[joystick].getRawAxis(leftTrigger);
-        return deadzone(valueLeftTrigger);
-    }
-
-    public double getRightTrigger(int joystick) {
-        double valueRightTrigger = sticks[joystick].getRawAxis(rightTrigger);
-        return deadzone(valueRightTrigger);
-    }
-
-    public Button getButton(int joystick, int button) {
-        return buttons[joystick][button];
+    public Button getButton(int joystick, Buttons button) {
+        return buttons[joystick][button.getValue()];
     }
 }
